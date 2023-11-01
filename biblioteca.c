eb76a04eed45228062d98107034441165cb61c15
Letizia L. Baptistella <145225061+le-bap@users.noreply.github.com>
2023-11-01 17:54:10 +0000

    comentários de como funciona cada função na biblioteca.c e na main.c finalizados

--- biblioteca.c
+++ biblioteca.c
@@ -10,9 +10,9 @@ void printMenu(){ // usada para printar o menu toda vez que acontece alguma aç�
 }
 
 
-int criarTarefa(ListaDeTarefas *lt) {
+int criarTarefa(ListaDeTarefas *lt) { // usada para criar uma nova terefa
 
-    if (lt->qtd < 100) { // se a lista tiver menos que 100 tarefas, é possivel configurar uma
+    if (lt->qtd < 100) { // se a lista tiver menos que 100 tarefas, a função pede a categoria, a desrição, a prioridade e o status da nova tarefa e armazena no struct
         printf("\nDigite a categoria: ");
         scanf(" %[^\n]", lt->tarefas[lt->qtd].categoria);
         clearBuffer();
@@ -31,30 +31,36 @@ int criarTarefa(ListaDeTarefas *lt) {
 
         printf("Tarefa adicionada com sucesso!\n");
     }
-    else{
+    else{ // caso o struct ja tenha 100 tarefas
         printf("Sua lista de tarefas esta cheia!");
     }
     lt->qtd++;// estrutura que pula pra proxima linha do arquivo para que o usuario nao salve as novas tarefas em cima das outras
     return 0;
 }
 
-int DeletarTarefa(ListaDeTarefas *lt){
+int DeletarTarefa(ListaDeTarefas *lt){ // função que deleta uma terefa
     int tarefaEscolhida;
-    printf("\nQual tarefa voce deseja deletar? (0 a 99) "); // informa a tarefa a ser deletada
+    printf("\nDigite a terefa que deseja deletar (de 0 a %d): ",lt->qtd - 1); // o usuario informa a tarefa a ser deletada
     scanf("%d", &tarefaEscolhida);
     clearBuffer();
 
-    for (int i = tarefaEscolhida; i < 100; i++){
+    if(tarefaEscolhida < lt->qtd){ // confere se a tarefa digitada existe
+        printf("Digite um numero valido.");
+        return 1;
+    }
+
+    for (int i = tarefaEscolhida; i < 100; i++){ // ao deletar uma tarefa, as posições das restantes devem "descer" para uma posição abaixo da que estava
         lt->tarefas[i] = lt->tarefas[i + 1];
     }
 
-    lt->qtd--;// ao deletar uma tarefa, as posições das restante devem "descer" para uma posição abaixo da que estava
+    lt->qtd--; // atualiza a quantidade de tarefas salvas
     return 0;
 }
 
 
-int ListarTarefa(ListaDeTarefas lt){
-    for (int i = 0; i < lt.qtd; i++){ //apenas aparece as tarefas já estabelecidas
+int ListarTarefa(ListaDeTarefas lt){ // percorre toda a lista de tarefas e printa no terminal
+    
+    for (int i = 0; i < lt.qtd; i++){ 
         printf("Tarefa %d\n", i);
         printf("Categoria: %s\n", lt.tarefas[i].categoria);
         printf("Descricao: %s\n", lt.tarefas[i].descricao);
@@ -62,30 +68,31 @@ int ListarTarefa(ListaDeTarefas lt){
         printf("Status: %s\n", lt.tarefas[i].status);
         printf("\n");
     }
+    
     return 0;
 }
 
-int EditarTarefa(ListaDeTarefas *lt){
+int EditarTarefa(ListaDeTarefas *lt){ // permeite alterar as configurações das tarefas existentes
 
     int tarefaEscolhida;
-    printf("\nDigite o numero da tarefa que deseja editar (0 a 99): ");
+    printf("\nDigite o numero da tarefa que deseja editar (0 a %d): ", lt->qtd - 1); // soliciata a tarefa a ser editada
     scanf("%d", &tarefaEscolhida);
     clearBuffer();
 
-    if (tarefaEscolhida > lt->qtd){
+    if (tarefaEscolhida > lt->qtd){ // verifica se ela existe
         printf("Digite um numero valido.");
         return 1;
     }
 
     else{
         int campoAlteracao;
-        printf("\nDigite o campo que deseja alterar:\n1. Categoria\n2. Descricao\n3. Prioridade\n4. Status\n>>> ");
+        printf("\nDigite o campo que deseja alterar:\n1. Categoria\n2. Descricao\n3. Prioridade\n4. Status\n>>> "); // solicita o configuração da tarefa a ser editada
         scanf("%d", &campoAlteracao);
         clearBuffer();
 
         if (campoAlteracao == 1){
             printf("Digite a nova categoria: ");
-            scanf("%[^\n]",  lt->tarefas[tarefaEscolhida].categoria);
+            scanf("%[^\n]",  lt->tarefas[tarefaEscolhida].categoria); // sobrescreve a informação que estava pela nova
             clearBuffer();
         }
         else if (campoAlteracao == 2){
@@ -107,19 +114,19 @@ int EditarTarefa(ListaDeTarefas *lt){
     return 0;
 }
 
-int Filtrar_Prioridade(ListaDeTarefas lt){
+int Filtrar_Prioridade(ListaDeTarefas lt){ // seleciona as tarefas que possuem a prioridade escolhida pelo usuario
     
     int prioridadeEscolhida;
-    printf("\nDigite o taxa de prioridade (0 a 10): ");
+    printf("\nDigite o taxa de prioridade (0 a 10): "); // pede a prioridade
     scanf("%d", &prioridadeEscolhida);
     clearBuffer();
 
-    if (prioridadeEscolhida > 10 || prioridadeEscolhida < 0){
+    if (prioridadeEscolhida > 10 || prioridadeEscolhida < 0){ // verifica se é um npumero plausível
         printf("Digite um numero valido.");
         return 1;
     }
     else{
-        for(int i = 0; i < lt.qtd; i++){
+        for(int i = 0; i < lt.qtd; i++){ // percorre a lista de tarefa e compara cada tarefa com a prioridade escolhida. Se forem compatíveis, aparece no terminal
             if (lt.tarefas[i].prioridade == prioridadeEscolhida){
                 printf("\n");
                 printf("Categoria: %s\n", lt.tarefas[i].categoria);
@@ -131,19 +138,19 @@ int Filtrar_Prioridade(ListaDeTarefas lt){
     }
 }
 
-int Filtrar_Estado(ListaDeTarefas lt){
-    
+int Filtrar_Estado(ListaDeTarefas lt){ // seleciona as tarefas que possuem o estado escolhido pelo usuario
+
     int estadoEscolhido;
-    printf("\nDigite o estado:\n1. nao iniciado\n2. em andamento\n3. completo\n>>> ");
+    printf("\nDigite o estado:\n1. nao iniciado\n2. em andamento\n3. completo\n>>> "); // pede o estado que se quer ver as tarefas
     scanf("%d", &estadoEscolhido);
     clearBuffer();
 
-    if (estadoEscolhido > 3 || estadoEscolhido < 1){
+    if (estadoEscolhido > 3 || estadoEscolhido < 1){ // verifica se a informação dada é uma das oferecidas no menu
         printf("Digite um numero valido.");
         return 1;
     }
     else{
-        for(int i = 0; i < lt.qtd; i++){
+        for(int i = 0; i < lt.qtd; i++){ // percorre toda a lista e compara se a tarefa possui o estado desejado pelo usuario. Se tiver, printa no terminal
             if (estadoEscolhido == 1 && strcmp(lt.tarefas[i].status, "nao iniciado") == 0){
                 printf("\n");
                 printf("Categoria: %s\n", lt.tarefas[i].categoria);
@@ -201,10 +208,10 @@ int Filtrar_Categoria(ListaDeTarefas lt){
     return 0;  
 }
 
-int Filtrar_Prioridade_Categoria(ListaDeTarefas lt){
+int Filtrar_Prioridade_Categoria(ListaDeTarefas lt){ // junta as funções de filtrar por categoria e filtrar por prioridade
 
     char categoriaEscolhida[100];
-    printf("\nDigite a categoria: ");
+    printf("\nDigite a categoria: "); // primeiro, pede a categoria
     scanf("%[^\n]", categoriaEscolhida);
     clearBuffer();
 
@@ -215,7 +222,7 @@ int Filtrar_Prioridade_Categoria(ListaDeTarefas lt){
     for (int i = 0; i < lt.qtd; i++){ // percorre toda a lista de tarefas
         if (strcmp(lt.tarefas[i].categoria, categoriaEscolhida) == 0){ 
             verificar = 1;
-            tr[total] = lt.tarefas[i]; // cria um lista com as tarefas da categoria escolhida
+            tr[total] = lt.tarefas[i]; // cria um lista com as tarefas da categoria escolhida (tr)
             total++;
         }
     }
@@ -225,17 +232,17 @@ int Filtrar_Prioridade_Categoria(ListaDeTarefas lt){
     }
 
     int prioridadeEscolhida;
-    printf("Digite o taxa de prioridade (0 a 10): ");
+    printf("Digite o taxa de prioridade (0 a 10): "); // depois, filtra por prioridade 
     scanf("%d", &prioridadeEscolhida);
     clearBuffer();
 
-    if (prioridadeEscolhida > 10 || prioridadeEscolhida < 0){
+    if (prioridadeEscolhida > 10 || prioridadeEscolhida < 0){ // verifica se a prioridade forncedia é válida
         printf("Digite um numero valido.");
         return 1;
     }
 
     else{
-        for(int i = 0; i < total + 1; i++){
+        for(int i = 0; i < total + 1; i++){ // dentro da nova lista de tarefas que passaram no filtro da categoria (tr), verifica qual possui a prioridade que o usuario deseja
             if (tr[i].prioridade == prioridadeEscolhida){
                 printf("\n");
                 printf("Descricao: %s\n", tr[i].descricao);
@@ -248,9 +255,9 @@ int Filtrar_Prioridade_Categoria(ListaDeTarefas lt){
 }
 
 
-int Exportar_Prioridade(ListaDeTarefas *lt){
+int Exportar_Prioridade(ListaDeTarefas *lt){ // exporta as tarefas de uma determinada prioridade para um documento txt
     
-    int prioridadeEscolhida;
+    int prioridadeEscolhida; // idem a função de filtrar por prioridade
     printf("\nDigite o taxa de prioridade (0 a 10): ");
     scanf("%d", &prioridadeEscolhida);
     clearBuffer();
@@ -261,7 +268,7 @@ int Exportar_Prioridade(ListaDeTarefas *lt){
     }
     else{
         
-        FILE *arq = fopen("Tarefas_Prioridade.txt", "w");
+        FILE *arq = fopen("Tarefas_Prioridade.txt", "w"); // ao inves de printar no terminal, escreve no arquivo Tarefas_Prioridade.txt
 
         for(int i = 0; i < lt->qtd; i++){// le as informações que estão em "lt" 
             if (lt->tarefas[i].prioridade == prioridadeEscolhida){
@@ -278,7 +285,7 @@ int Exportar_Prioridade(ListaDeTarefas *lt){
     
 }
 
-int Exportar_Prioridade_Categoria(ListaDeTarefas *lt){
+int Exportar_Prioridade_Categoria(ListaDeTarefas *lt){ // exporta as tarefas de uma determinada categoria e prioridade para um documento txt
     
     char categoriaEscolhida[100];
     printf("\nDigite a categoria: ");
@@ -302,11 +309,11 @@ int Exportar_Prioridade_Categoria(ListaDeTarefas *lt){
     }
 
     int prioridadeEscolhida;
-    printf("Digite o taxa de prioridade (0 a 10): ");
+    printf("Digite o taxa de prioridade (0 a 10): "); // solicita a prioridade
     scanf("%d", &prioridadeEscolhida);
     clearBuffer();
 
-    if (prioridadeEscolhida > 10 || prioridadeEscolhida < 0){
+    if (prioridadeEscolhida > 10 || prioridadeEscolhida < 0){ // verifica se é uma prioridade válida
         printf("Digite um numero valido.");
         return 1;
     }
@@ -315,7 +322,7 @@ int Exportar_Prioridade_Categoria(ListaDeTarefas *lt){
         FILE *arq = fopen("Tarefas_Prioridade_Categoria.txt", "w");
 
         for(int i = 0; i < total; i++){
-            if (tr[i].prioridade == prioridadeEscolhida){
+            if (tr[i].prioridade == prioridadeEscolhida){ // confere, dentro da nova lista (tr), quais possuem a prioridade informada e escreve no documento
                 fprintf(arq,"Prioridade: %d; " ,tr[i].prioridade);
                 fprintf(arq,"Categoria: %s; " ,tr[i].categoria);
                 fprintf(arq,"Status: %s; " ,tr[i].status);
@@ -329,7 +336,6 @@ int Exportar_Prioridade_Categoria(ListaDeTarefas *lt){
 }
 
 
-
 ///////////// funções base /////////////
 void clearBuffer(){ //evita erros com a função scanf
     int c;
@@ -338,7 +344,7 @@ void clearBuffer(){ //evita erros com a função scanf
 
 
 ///////////// funções relacionadas ao arquivo /////////////
-int salvarLista(ListaDeTarefas *lt, char nome[]){ // "escreve" em uma arquivo a lista de tarefas em binário para salvá-la
+int salvarLista(ListaDeTarefas *lt, char nome[]){ // "escreve" em um arquivo a lista de tarefas em binário para salvá-la
     FILE *f = fopen(nome, "wb");
     if(f == NULL){
         return 1;
